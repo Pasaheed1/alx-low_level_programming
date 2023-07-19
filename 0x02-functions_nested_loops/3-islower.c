3b8bcf20cb1ecce3f429564605efec1089d1f3fc
Pasaheed1 <saheedadewalealli@gmail.com>
2023-07-18 20:33:42 -0700

    C - Functions, nested loops Task 3

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _islower - main function
+ * @c: paarameter to check
+ *
+ * Return: 1 or 0
+ */
+
+int _islower(int c)
+{
+	if (c >= 'a' && c <= 'z')
+		return (1);
+	else
+		return (0);
+}
